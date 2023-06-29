4c438d378a6a858dd8cefe395fdc079383e1ed63
Putu Gde Arya Bhanuartha <tuaryabhanu03@gmail.com>
2023-06-29 20:36:34 +0700

    delete cache file

--- src/pages/ProfilDesa/Peta/osmProvider.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    mapTiler : {
-        attribution : "&copy; <a href=\"https://www.maptiler.com/copyright/\" target=\"_blank\">&copy; MapTiler</a> <a href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\">&copy; OpenStreetMap contributors</a>",
-        url : "https://api.maptiler.com/maps/topo-v2/{z}/{x}/{y}@2x.png?key=DSvz93fQjE8ieHP7iPkP"
-    }
-}
